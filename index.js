fafd3431cb6d53645f02157d955aaaabea896ee6
Bill Brown <bill.a.brown90@gmail.com>
2023-02-24 10:13:00 -0800

    Using DOMContentLoaded

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOM has Loaded")
+    document.querySelector("#text").textContent = "This is really cool!"
+})
+
+console.log("just checking that this is before")
